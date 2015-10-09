f87587d2543b909c65ed63a3de99a8725e746773
gengsun <gengsun@gmail.com>
2015-10-09 14:30:33 +0100

    added SpringTest

---
+++ SpringFramework/src/main/java/sun/spring/SpringTest.java
@@ -0,0 +1,12 @@
+package sun.spring;
+
+/**
+ * Created by sun on 09/10/15.
+ */
+public class SpringTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Spring Framework test...");
+    }
+}
