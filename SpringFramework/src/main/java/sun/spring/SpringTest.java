a9d29c187108911b16aaaebff0c3068e0f485d80
gengsun <gengsun@gmail.com>
2015-10-09 17:01:23 +0100

    first spring application

--- SpringFramework/src/main/java/sun/spring/SpringTest.java
+++ SpringFramework/src/main/java/sun/spring/SpringTest.java
@@ -1,14 +1,24 @@
 package sun.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by sun on 09/10/15.
  */
 public class SpringTest
 {
-
+    public static void print()
+    {
+        System.out.println("Spring Framework test...");
+    }
 
     public static void main(String[] args)
     {
-        System.out.println("Spring Framework test...");
+        print();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
     }
 }
