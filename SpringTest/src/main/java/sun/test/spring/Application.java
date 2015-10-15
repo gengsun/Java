635a7db853c00ce43b603bc9e960152cb5d99a30
gengsun <gengsun@gmail.com>
2015-10-15 11:42:17 +0100

    minor change in Application.java to test differences between head, master, and origin master

--- SpringTest/src/main/java/sun/test/spring/Application.java
+++ SpringTest/src/main/java/sun/test/spring/Application.java
@@ -22,8 +22,6 @@ public class Application
 
     public static void main(String[] args)
     {
-
-
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
         printer.printMessage();
