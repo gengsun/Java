38099405a5287737610a625fbb7502c12ad5775a
gengsun <gengsun@gmail.com>
2015-10-15 10:25:19 +0100

    added log4j.properties to stop default console spring logging

--- SpringTest/src/main/java/sun/test/spring/Application.java
+++ SpringTest/src/main/java/sun/test/spring/Application.java
@@ -22,6 +22,8 @@ public class Application
 
     public static void main(String[] args)
     {
+
+
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
         printer.printMessage();
