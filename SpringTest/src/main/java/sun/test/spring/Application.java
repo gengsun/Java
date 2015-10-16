419e21f54c5b05428f8687cf9ff0e864433d4e2e
gengsun <gengsun@gmail.com>
2015-10-16 13:24:39 +0100

    changed to customised assembly descriptor

--- SpringTest/src/main/java/sun/test/spring/Application.java
+++ SpringTest/src/main/java/sun/test/spring/Application.java
@@ -22,21 +22,20 @@ public class Application
 
     public static void main(String[] args)
     {
-//        AbstractApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-//        MessagePrinter printer = context.getBean(MessagePrinter.class);
-//        printer.printMessage();
+        AbstractApplicationContext applicationContext = new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = applicationContext.getBean(MessagePrinter.class);
+        printer.printMessage();
 
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 
-//        handler.setMessage("I am bean A");
-//        handler.getMessage();
-//
-//        BeansXMLHandler handler1 = (BeansXMLHandler) context.getBean("helloWorld");
-//        handler1.setMessage("I am bean B");
-//        handler1.getMessage();
+//        AbstractApplicationContext context1 = new ClassPathXmlApplicationContext("Beans.xml");
+//        HelloWorld obj1 = (HelloWorld) context1.getBean("helloWorld");
 
         obj.getMessage();
+//        obj1.setMessage("changed message...");
+//        obj1.getMessage();
+//        obj.getMessage();
 
         context.registerShutdownHook();
     }
