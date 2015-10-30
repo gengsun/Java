60477bba93e90cf4fe03330f8558bfaee9f8fb0a
gengsun <gengsun@gmail.com>
2015-10-30 15:28:16 +0000

    Spring MVC (not working)

---
+++ SpringTest/src/main/java/sun/java/spring/mvc/HelloController.java
@@ -0,0 +1,22 @@
+package sun.java.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by sun on 29/10/15.
+ */
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController
+{
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap modelMap)
+    {
+        modelMap.addAttribute("message", "Hello Spring MVC Framework");
+        return "hello";
+    }
+}
