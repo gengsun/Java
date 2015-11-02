6bd7839d9f2824a649f7553f7e2cbfcbda43cb6a
gengsun <gengsun@gmail.com>
2015-11-02 10:19:32 +0000

    first commit for Hibernate

---
+++ Hibernate/src/main/java/sun/java/hibernate/Main.java
@@ -0,0 +1,12 @@
+package sun.java.hibernate;
+
+/**
+ * Created by sun on 02/11/15.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+    }
+}
