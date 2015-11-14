130f9622023e9b657e22bf40448ab2fc175201ce
gengsun <gengsun@gmail.com>
2015-11-14 00:11:14 +0000

    Hello World mvc with JBoss log file

--- SpringTest/src/main/java/sun/java/spring/mvc/controller/AppController.java
+++ SpringTest/src/main/java/sun/java/spring/mvc/controller/AppController.java
@@ -1,8 +1,22 @@
 package sun.java.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by sun on 12/11/15.
  */
+
+@Controller
+@RequestMapping("/hello")
 public class AppController
 {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap modelMap)
+    {
+        modelMap.addAttribute("message", "Sun's Spring MVC Hello World Example.");
+        return "hello";
+    }
 }
